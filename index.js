6bb5aca0b9e431a675e2dc715881d046197873a5
AmericanThor <105684121+AmericanThor@users.noreply.github.com>
2023-03-24 15:00:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const routes = require('./router/friends.js')
+
+let users = []
+
+//Avoids duplication & keeps username unique. This is not an endpoint.
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Checks for matching Username/Passwords. Returns boolean on credentials. Utility function - not an endpoint.
+
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const app = express();
+
+//Middleware to intercept request and ensure session is valid before the request.
+
+app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+
+app.use(express.json());
+
+//Sends endpoint through middleware to restrict to authenticed
+
+app.use("/friends", function auth(req,res,next){
+   if(req.session.authorization) {
+       token = req.session.authorization['accessToken'];
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
+
+
+//FINAL PROJECT ___Returns error if no UN/PW is provided, creates token that lasts (60sX60) 1 hour, throws error if credentials are incorrect.
+
+app.post("/login", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
+});
+
+//This is the way ...to register the users. An endpoint that doesn't need to be authenticated. 
+
+app.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+
+const PORT =5000;
+
+app.use("/friends", routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
